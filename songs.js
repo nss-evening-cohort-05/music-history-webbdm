6ae7ed2eaf3f29aac9356e9eee37b5374c0ff08b
webbdm <gwebb762@gmail.com>
2017-02-16 23:48:33 -0600

    added replace

--- songs.js
+++ songs.js
@@ -9,8 +9,8 @@ songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little
 console.log(songs.length);
 
 
-var newSong1 = "Hotel California by the Eagles";
-var newSong2 = "Thriller by Michael Jackson";
+var newSong1 = "Hotel California > by the Eagles";
+var newSong2 = "Thriller > by Michael Jackson";
 
 songs.unshift(newSong1);
 songs.push(newSong2);
@@ -19,7 +19,17 @@ console.log(songs);
 
 for (i=0; i < songs.length; i++){
 
-	songs[i] = songs[i].replace(">","");
-	songs[i] = songs[i].replace("","");
+	songs[i] = songs[i].replace(/>/g, '-');
+
+	// songs[i] = songs[i].replace(/[\W_]+/g," ");
+
+}
+
+
+var songAdd = document.getElementById('songs');
+
+for (var i = 0; i < songs.length; i++) {
+	
+	songAdd.innerHTML += '<li>' + songs[i] + '</li>';
 
 }
