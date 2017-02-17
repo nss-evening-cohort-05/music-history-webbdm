23bb823b8ffaf26c74246680fb6c9a47ca7cc2f3
webbdm <gwebb762@gmail.com>
2017-02-16 21:29:18 -0600

    added push and pop

--- songs.js
+++ songs.js
@@ -6,4 +6,20 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
+console.log(songs.length);
 
+
+var newSong1 = "Hotel California by the Eagles";
+var newSong2 = "Thriller by Michael Jackson";
+
+songs.unshift(newSong1);
+songs.push(newSong2);
+
+console.log(songs);
+
+for (i=0; i < songs.length; i++){
+
+	songs[i] = songs[i].replace(">","");
+	songs[i] = songs[i].replace("","");
+
+}
