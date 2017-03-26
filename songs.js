145af38b75e72ec7520386afac1205dece9de8f8
webbdm <gwebb762@gmail.com>
2017-03-25 21:39:13 -0500

    Added SASS and began refactoring with jQuery

--- songs.js
+++ songs.js
@@ -1,35 +1,88 @@
-var songs = [];
+// $(document).ready(function() {
+//     $.ajax({
+//         url: "songs.json",
+//         data: {
+//             format: 'json'
+//         },
+//         succes: function() {
+//             console.log("Songs.json has been loaded");
+//         },
+//         error: function() {
+//             console.log("Songs.json failed to load");
+//         },
+//         dataType: 'json',
+//         type: "GET"
+//     });
+// });
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+var songs = [
+    {
+        "name": "Hotel California",
+        "artist": "The Eagles",
+        "album": "Hotel California"
+    }, {
+        "name": "Purple Haze",
+        "artist": "Jimi Hendrix",
+        "album": "Live at Woodstock"
+    }, {
+        "name": "Welcome to the Jungle",
+        "artist": "Guns N' Roses",
+        "album": "Appetite for Destruction"
+    }, {
+        "name": "Thriller",
+        "artist": "Michael Jackson",
+        "album": "Thriller"
+    }, {
+        "name": "Song 2",
+        "artist": "Blur",
+        "album": "Blur"
+    }, {
+        "name": "Ironic",
+        "artist": "Alanis Morisette",
+        "album": "Jagged Little Pill"
+    }, {
+        "name": "Legs",
+        "artist": "ZZ Top",
+        "album": "Eliminator"
+    }, {
+        "name": "Smells Like Teen Spirit",
+        "artist": "Nirvana",
+        "album": "Nevermind"
+    }
+];
 
-console.log(songs.length);
-
-
-var newSong1 = "Hotel California > by the Eagles";
-var newSong2 = "Thriller > by Michael Jackson";
-
-songs.unshift(newSong1);
-songs.push(newSong2);
+// var songs = [];
+//
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+//
+// console.log(songs.length);
+//
+// var newSong1 = "Hotel California > by the Eagles";
+// var newSong2 = "Thriller > by Michael Jackson";
+//
+// songs.unshift(newSong1);
+// songs.push(newSong2);
 
 console.log(songs);
 
-for (i=0; i < songs.length; i++){
-
-	songs[i] = songs[i].replace(/>/g, '-');
-
-	// songs[i] = songs[i].replace(/[\W_]+/g," ");
-
-}
-
-
-var songAdd = document.getElementById('songs');
+// for (i = 0; i < songs.length; i++) {
+//
+//     songs[i] = songs[i].replace(/>/g, '-');
+//     songs[i] = songs[i].replace(/\*/g, "");
+//     songs[i] = songs[i].replace(/@/g, '-');
+//     songs[i] = songs[i].replace(/\(/g, '');
+//     songs[i] = songs[i].replace(/!/g, '');
+//
+//     // songs[i] = songs[i].replace(/[\W_]+/g," ");
+//
+// }
 
 for (var i = 0; i < songs.length; i++) {
-	
-	songAdd.innerHTML += '<li>' + songs[i] + '</li>';
+
+    $("#songs").html(`<li>${songs[i].name} by ${songs[i].artist} from the album ${songs[i].album}</li> <button>DELETE</button>`);
 
 }
