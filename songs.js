de256d8037e83e32a349d3c9b0a827317d1452a3
webbdm <gwebb762@gmail.com>
2017-03-26 23:30:23 -0500

    added xhr request and printSongs function

--- songs.js
+++ songs.js
@@ -1,66 +1,29 @@
-var songs = [];
+//var songs = [];
+var list = document.getElementById('songs');
 
+function printSongs(data) {
 
+    var songData = data.songs;
+    songData.map(function(song) {
+        list.innerHTML += `<li> ${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`;
+    });
 
+}
 
+function success() {
+    var data = JSON.parse(this.responseText);
+    console.log('success');
+    printSongs(data);
+}
 
+function failed() {
+    console.log("failed");
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-//
-// console.log(songs.length);
-//
-//
-// var newSong1 = "Hotel California > by the Eagles";
-// var newSong2 = "Thriller > by Michael Jackson";
-//
-// songs.unshift(newSong1);
-// songs.push(newSong2);
-
-// console.log(songs);
-//
-// for (i=0; i < songs.length; i++){
-//
-// 	songs[i] = songs[i].replace(/>/g, '-');
-//
-// 	// songs[i] = songs[i].replace(/[\W_]+/g," ");
-//
-// }
-//
-//
-// var songAdd = document.getElementById('songs');
-//
-// for (var i = 0; i < songs.length; i++) {
-//
-// 	songAdd.innerHTML += '<li>' + songs[i] + '</li>';
-//
-// }
+var xhr = new XMLHttpRequest();
+xhr.addEventListener("load", success);
+xhr.addEventListener("error", failed);
+xhr.open("GET", "songs.json");
+xhr.send();
+console.log("xhr", xhr);
