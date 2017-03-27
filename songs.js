ece787b3eb5e3248971722723a05e1d1a2f6b02a
webbdm <gwebb762@gmail.com>
2017-03-26 22:08:56 -0500

    setup sass

--- songs.js
+++ songs.js
@@ -1,35 +1,66 @@
 var songs = [];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
-console.log(songs.length);
 
 
-var newSong1 = "Hotel California > by the Eagles";
-var newSong2 = "Thriller > by Michael Jackson";
-
-songs.unshift(newSong1);
-songs.push(newSong2);
-
-console.log(songs);
-
-for (i=0; i < songs.length; i++){
-
-	songs[i] = songs[i].replace(/>/g, '-');
-
-	// songs[i] = songs[i].replace(/[\W_]+/g," ");
-
-}
 
 
-var songAdd = document.getElementById('songs');
 
-for (var i = 0; i < songs.length; i++) {
-	
-	songAdd.innerHTML += '<li>' + songs[i] + '</li>';
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+//
+// console.log(songs.length);
+//
+//
+// var newSong1 = "Hotel California > by the Eagles";
+// var newSong2 = "Thriller > by Michael Jackson";
+//
+// songs.unshift(newSong1);
+// songs.push(newSong2);
+
+// console.log(songs);
+//
+// for (i=0; i < songs.length; i++){
+//
+// 	songs[i] = songs[i].replace(/>/g, '-');
+//
+// 	// songs[i] = songs[i].replace(/[\W_]+/g," ");
+//
+// }
+//
+//
+// var songAdd = document.getElementById('songs');
+//
+// for (var i = 0; i < songs.length; i++) {
+//
+// 	songAdd.innerHTML += '<li>' + songs[i] + '</li>';
+//
+// }
