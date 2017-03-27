215748c9fc8e38d03fe8516e3270acd225f0928b
webbdm <gwebb762@gmail.com>
2017-03-26 23:48:00 -0500

    added delete button functionality to songs

--- songs.js
+++ songs.js
@@ -2,23 +2,19 @@
 var list = document.getElementById('songs');
 
 function printSongs(data) {
-
     var songData = data.songs;
-    songData.map(function(song) {
-        list.innerHTML += `<li> ${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`;
+    songData.map(function(song, index) {
+        list.innerHTML += `<li id="${index}"> ${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`;
     });
-
 }
 
 function success() {
     var data = JSON.parse(this.responseText);
-    console.log('success');
     printSongs(data);
 }
 
 function failed() {
-    console.log("failed");
-
+    alert("song data failed to load");
 }
 
 var xhr = new XMLHttpRequest();
@@ -27,3 +23,9 @@ xhr.addEventListener("error", failed);
 xhr.open("GET", "songs.json");
 xhr.send();
 console.log("xhr", xhr);
+
+document.body.addEventListener("click", function(event) {
+    if (event.target.className === "delete") {
+        event.target.parentNode.remove();
+    }
+});
