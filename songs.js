83e96748f1bb418fd69701766e1ae4d6faa13a9a
webbdm <gwebb762@gmail.com>
2017-03-27 00:19:23 -0500

    Added a 2nd XHR request

--- songs.js
+++ songs.js
@@ -8,13 +8,14 @@ function printSongs(data) {
     });
 }
 
+// XHR for 1st Songs JSON file
 function success() {
     var data = JSON.parse(this.responseText);
     printSongs(data);
 }
 
 function failed() {
-    alert("song data failed to load");
+    alert("Song data failed to load");
 }
 
 var xhr = new XMLHttpRequest();
@@ -24,6 +25,20 @@ xhr.open("GET", "songs.json");
 xhr.send();
 console.log("xhr", xhr);
 
+// XHR for 2nd Songs JSON file
+function successTwo() {
+    var data = JSON.parse(this.responseText);
+    printSongs(data);
+}
+
+var xhr = new XMLHttpRequest();
+xhr.addEventListener("load", successTwo);
+xhr.addEventListener("error", failed);
+xhr.open("GET", "songs2.json");
+xhr.send();
+console.log("xhr", xhr);
+
+// Delete button functionality for cards
 document.body.addEventListener("click", function(event) {
     if (event.target.className === "delete") {
         event.target.parentNode.remove();
