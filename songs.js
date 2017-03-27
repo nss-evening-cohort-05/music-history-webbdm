20e931eb0184e69b5638e82ef8b442ba50bcb975
webbdm <gwebb762@gmail.com>
2017-03-26 22:17:19 -0500

    added Firebase url to ajax

--- songs.js
+++ songs.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "../songs.json",
+        url: "https://music-history-28bea.firebaseio.com/.json",
         success: function() {
             console.log("Songs.json has been loaded");
         },
@@ -9,45 +9,45 @@ $(document).ready(function() {
         },
         dataType: 'json',
         type: "GET",
-        async: false
+        //async: false
     });
 });
 
-var songs = [
-    {
-        "name": "Hotel California",
-        "artist": "The Eagles",
-        "album": "Hotel California"
-    }, {
-        "name": "Purple Haze",
-        "artist": "Jimi Hendrix",
-        "album": "Live at Woodstock"
-    }, {
-        "name": "Welcome to the Jungle",
-        "artist": "Guns N' Roses",
-        "album": "Appetite for Destruction"
-    }, {
-        "name": "Thriller",
-        "artist": "Michael Jackson",
-        "album": "Thriller"
-    }, {
-        "name": "Song 2",
-        "artist": "Blur",
-        "album": "Blur"
-    }, {
-        "name": "Ironic",
-        "artist": "Alanis Morisette",
-        "album": "Jagged Little Pill"
-    }, {
-        "name": "Legs",
-        "artist": "ZZ Top",
-        "album": "Eliminator"
-    }, {
-        "name": "Smells Like Teen Spirit",
-        "artist": "Nirvana",
-        "album": "Nevermind"
-    }
-];
+// var songs = [
+//     {
+//         "name": "Hotel California",
+//         "artist": "The Eagles",
+//         "album": "Hotel California"
+//     }, {
+//         "name": "Purple Haze",
+//         "artist": "Jimi Hendrix",
+//         "album": "Live at Woodstock"
+//     }, {
+//         "name": "Welcome to the Jungle",
+//         "artist": "Guns N' Roses",
+//         "album": "Appetite for Destruction"
+//     }, {
+//         "name": "Thriller",
+//         "artist": "Michael Jackson",
+//         "album": "Thriller"
+//     }, {
+//         "name": "Song 2",
+//         "artist": "Blur",
+//         "album": "Blur"
+//     }, {
+//         "name": "Ironic",
+//         "artist": "Alanis Morisette",
+//         "album": "Jagged Little Pill"
+//     }, {
+//         "name": "Legs",
+//         "artist": "ZZ Top",
+//         "album": "Eliminator"
+//     }, {
+//         "name": "Smells Like Teen Spirit",
+//         "artist": "Nirvana",
+//         "album": "Nevermind"
+//     }
+// ];
 
 songs.map(function(song) {
     $("#songs").append(`<li>${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`);
