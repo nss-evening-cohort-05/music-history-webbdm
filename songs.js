37d77f246e02d64a7db5f32174943ddd1e06ab52
webbdm <gwebb762@gmail.com>
2017-04-05 23:32:56 -0500

    AJAX is functional

--- songs.js
+++ songs.js
@@ -1,73 +1,15 @@
-// $(document).ready(function() {
-//
-//   $.ajax({
-//     url: "data/songs.json"
-//   }).done(function(contentsOfTheFile) {
-//     // When you tell jQuery to read a file via the ajax method
-//     // it reads the contents, and then executes whatever function
-//     // that you specify here in the done() method, and passes in
-//     // the contents of the file as the first argument.
-//     console.log("the contents of songs.json");
-//     console.log(contentsOfTheFile);
-//   });
-//
-// });
+var songArray = [];
 
-// $(document).ready(function() {
-//     $.ajax({
-//         url: "/songs.json",
-//         success: function() {
-//             console.log("Songs.json has been loaded");
-//         },
-//         error: function() {
-//             console.log("Songs.json failed to load");
-//         },
-//         dataType: 'json',
-//         type: "GET",
-//         //async: false
-//     });
-// });
-
-var songs = [
-    {
-        "name": "Hotel California",
-        "artist": "The Eagles",
-        "album": "Hotel California"
-    }, {
-        "name": "Purple Haze",
-        "artist": "Jimi Hendrix",
-        "album": "Live at Woodstock"
-    }, {
-        "name": "Welcome to the Jungle",
-        "artist": "Guns N' Roses",
-        "album": "Appetite for Destruction"
-    }, {
-        "name": "Thriller",
-        "artist": "Michael Jackson",
-        "album": "Thriller"
-    }, {
-        "name": "Song 2",
-        "artist": "Blur",
-        "album": "Blur"
-    }, {
-        "name": "Ironic",
-        "artist": "Alanis Morisette",
-        "album": "Jagged Little Pill"
-    }, {
-        "name": "Legs",
-        "artist": "ZZ Top",
-        "album": "Eliminator"
-    }, {
-        "name": "Smells Like Teen Spirit",
-        "artist": "Nirvana",
-        "album": "Nevermind"
-    }
-];
-
-songs.map(function(song) {
-    $("#songs").append(`<li>${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`);
-});
-
-$(".delete").click(function() {
-    $(this).parent().remove();
+$.ajax({url: "data/songs.json"}).done(function(data) {
+    var ajaxData = data.songs;
+    ajaxData.map(function(song, index) {
+        $("#songs").append(`<li>${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`);
+    });
+    $(".delete").click(function() {
+        $(this).parent().remove();
+    });
+}).fail(function(error) {
+    console.log("Failed", error);
+}).always(function() {
+    console.log("Test");
 });
