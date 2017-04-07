642f617ae820865ca3b67c8564a2deeb4a430a5a
webbdm <gwebb762@gmail.com>
2017-04-06 21:45:50 -0500

    added to AJAX function

--- songs.js
+++ songs.js
@@ -13,3 +13,21 @@ $.ajax({url: "data/songs.json"}).done(function(data) {
 }).always(function() {
     console.log("Test");
 });
+
+$("#more").click(function() {
+
+    $.ajax({url: "data/songs2.json"}).done(function(data) {
+        var ajaxData = data.songs;
+        ajaxData.map(function(song, index) {
+            $("#songs").append(`<li>${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`);
+        });
+        $(".delete").click(function() {
+            $(this).parent().remove();
+        });
+    }).fail(function(error) {
+        console.log("Failed", error);
+    }).always(function() {
+        console.log("Test");
+    });
+
+})
