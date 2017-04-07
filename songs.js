5eb3b0655e39e9078daa0f7d3586fe5af23124e3
webbdm <gwebb762@gmail.com>
2017-04-06 22:38:21 -0500

    Assignment Complete

--- songs.js
+++ songs.js
@@ -2,12 +2,7 @@ var songArray = [];
 
 $.ajax({url: "data/songs.json"}).done(function(data) {
     var ajaxData = data.songs;
-    ajaxData.map(function(song, index) {
-        $("#songs").append(`<li>${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`);
-    });
-    $(".delete").click(function() {
-        $(this).parent().remove();
-    });
+    addSongs(ajaxData);
 }).fail(function(error) {
     console.log("Failed", error);
 }).always(function() {
@@ -15,19 +10,23 @@ $.ajax({url: "data/songs.json"}).done(function(data) {
 });
 
 $("#more").click(function() {
-
     $.ajax({url: "data/songs2.json"}).done(function(data) {
         var ajaxData = data.songs;
-        ajaxData.map(function(song, index) {
-            $("#songs").append(`<li>${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`);
-        });
-        $(".delete").click(function() {
-            $(this).parent().remove();
-        });
+        addSongs(ajaxData);
     }).fail(function(error) {
         console.log("Failed", error);
     }).always(function() {
         console.log("Test");
     });
+    // Disable button
+    $(this).prop("disabled", true);
+});
 
-})
+function addSongs(data) {
+    data.map(function(song, index) {
+        $("#songs").append(`<li>${song.name} by ${song.artist} from the album ${song.album} <button class="delete">DELETE</button></li>`);
+    });
+    $(".delete").click(function() {
+        $(this).parent().remove();
+    });
+}
